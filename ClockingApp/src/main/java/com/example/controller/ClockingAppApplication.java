c855b0e666007368f92ac2d2a2ea05bef920eb23
Chad Thundercock <82008344+KunWardhana@users.noreply.github.com>
2023-10-06 13:04:17 +0700

    Add files via upload

---
+++ ClockingApp/src/main/java/com/example/controller/ClockingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockingAppApplication.class, args);
+	}
+
+}
